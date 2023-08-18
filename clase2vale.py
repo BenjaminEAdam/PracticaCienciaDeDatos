45294ffa5064144416a2b018303561e84923ad82
Valentin Fontana <valefontana15@gmail.com>
2023-08-18 17:33:15 -0300

    ejemplos de clase

--- clase2vale.py
+++ clase2vale.py
@@ -6,3 +6,18 @@ ps = set(txt.split(" "))
 i = range(0, len(ps))
 it = dict(zip(ps, i))
 print(it.get('abc'))
+
+## Compresion de listas
+
+y = [t**2 for t in i if t > 2] #devuelve la lista con los elementos al cuadrado para los t > 2
+print(y)
+
+## Bigrama con comprension de listas
+
+z = [x for x in zip(ps, ps[1:])]
+print(z)
+
+## Abrir archivo, w ubica el puntero al principio asi q sobreescribe el archivo.
+
+a = open('prueba.txt', 'w', encoding='utf-8')
+
