31504906fc0e664b9b20c445bec1376afdf0ccc1
BenjaminEAdam <benjaadam14@gmail.com>
2023-08-18 17:15:40 -0300

    redondeo con %

--- primera_clase.py
+++ primera_clase.py
@@ -1,11 +1,23 @@
+import numpy as np
+import math as mt
+
 lista=[0,1,2,3,4,5,6]
 
 print(lista)
 
-print("el 7 está en la lista?")
+print('el "7" está en la lista?')
 
 if(7 in lista):
     print("True")
 else:
     print("False")
+
+print(""" Puedo escribir 
+      saltos 
+      de linea! """)
+
+pi= mt.pi
+pi_redondeado= "%.2f" % pi
+print("Redondeo el numero pi:")
+print(pi_redondeado)
 
